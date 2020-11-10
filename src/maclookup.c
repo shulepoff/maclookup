e2e3267f0ebbe797b5e43fc4a434b40eebc5380a
Oleg Shulepov <shulepov@gmail.com>
2020-11-10 19:06:29 +0500

    another small changes

--- src/maclookup.c
+++ src/maclookup.c
@@ -7,73 +7,77 @@
 #define VERSION "0.3 alfa"
 
 struct globalArgs_t {
-	int updOui;       /* option -u */
-	int configure;    /* option -c */
-	int information;  /* option -i */
-	int usage;	      /* option -h */
-	char *macAddress; /* MAC */
+    int updOui;       /* option -u */
+    int configure;    /* option -c */
+    int information;  /* option -i */
+    int usage;          /* option -h */
+    char *macAddress; /* MAC */
 } globalArgs;
 
 static const char *optString = "ucihv";
 
 enum { Folder, Url, nKeys};
 char *keys[nKeys] = {
-	[Folder] = "folder",
-	[Url]	 = "url",
+    [Folder] = "folder",
+    [Url]     = "url",
 };
 char *config[nKeys];
 char *trim(char *s) {
-	char *p = s + strlen(s) -1;
-	while (isascii(*s) && isspace(*s))
-		s++;
-	while (isascii(*p) && isspace(*p))
-		*p-- = '\0';
-	return s;
+    char *p = s + strlen(s) -1;
+    while (isascii(*s) && isspace(*s))
+        s++;
+    while (isascii(*p) && isspace(*p))
+        *p-- = '\0';
+    return s;
 }
 
-void rdconf(FILE *fd) {
-	char buf[81], *s, *p;
-	char *key, *value;
-	int i;
+void read_conf(FILE *fd) 
+{
+    char buf[81], *s, *p;
+    char *key, *value;
+    int i;
 
-	while (fgets(buf, sizeof(buf), fd) != NULL) {
-		s = trim(buf);
-		/* got comment or empty line? go ahead */
-		if (*s == '#' || *s == '\0')
-			continue;
-		/* not a key=value pair? go ahead */
-		p = strchr(s, '=');
-		if (!p)
-			continue;
-		/* separate key and value */
-		*p++ = '\0';
-		/* trim again in case there was a whitespace around '=' */
-		key = trim(s);
-		value = trim(p);
-		/* and store it finally */
-		for (i = 0; i < nKeys; i++)
-			if (strcmp(keys[i], key) == 0)
-				config[i] = strdup(value);
-	}	
+    while (fgets(buf, sizeof(buf), fd) != NULL) {
+        s = trim(buf);
+        /* got comment or empty line? go ahead */
+        if (*s == '#' || *s == '\0')
+            continue;
+        /* not a key=value pair? go ahead */
+        p = strchr(s, '=');
+        if (!p)
+            continue;
+        /* separate key and value */
+        *p++ = '\0';
+        /* trim again in case there was a whitespace around '=' */
+        key = trim(s);
+        value = trim(p);
+        /* and store it finally */
+        for (i = 0; i < nKeys; i++)
+            if (strcmp(keys[i], key) == 0)
+                config[i] = strdup(value);
+    }    
 }
-void print_usage(FILE *handle) {
-	fputs("Usage: maclookup [-u][-v][-c][-i][-h] [MAC address]\n",handle);
+void print_usage(FILE *handle) 
+{
+    fputs("Usage: maclookup [-u][-v][-c][-i][-h] [MAC address]\n",handle);
 }
-char *mac_sanitize(char *mac) {
-	int i,j;
-	/* UPPERCASE that AND remove ':' and '-' */
-	for(i=0,j=0; mac[i]; i++) {
-		if(mac[i] >= 'a' && mac[i] <= 'z') {
-			mac[i] = mac[i]-32;
-		}
-		if (mac[i]==':' || mac[i]=='-') continue;
-		if (j<6) mac[j] = mac[i]; else mac[j]=0 ;
-		j++;
-	}
-	mac[j]=0;
-	return mac;
+char *mac_sanitize(char *mac) 
+{
+    int i,j;
+    /* UPPERCASE that AND remove ':' and '-' */
+    for(i=0,j=0; mac[i]; i++) {
+        if(mac[i] >= 'a' && mac[i] <= 'z') {
+            mac[i] = mac[i]-32;
+        }
+        if (mac[i]==':' || mac[i]=='-') continue;
+        if (j<6) mac[j] = mac[i]; else mac[j]=0 ;
+        j++;
+    }
+    mac[j]=0;
+    return mac;
 }
-void opncfg(void) {
+void open_conf(void) 
+{
      char *home, *filename;
      home = getenv("HOME");
      char *fname = "/.maclookup";
@@ -81,123 +85,128 @@ void opncfg(void) {
      FILE *f = fopen(filename,"r");
      if (!f) {
          f = fopen(filename,"w");
-		 fprintf(f, "folder = /usr/share/ieee-data/\n");
-		 fprintf(f, "url = https://linuxnet.ca/ieee/oui.txt.bz2\n");
+         fprintf(f, "folder = /usr/share/ieee-data/\n");
+         fprintf(f, "url = https://linuxnet.ca/ieee/oui.txt.bz2\n");
          printf("File %s created\n", filename);
-		 fclose(f);
-		 f = fopen(filename,"r");
+         fclose(f);
+         f = fopen(filename,"r");
      }
-	 rdconf(f);
+     read_conf(f);
      fclose(f);
-}	
-void print_info(void) {
-	printf("Folder ieee-data is %s \n",config[Folder]);
-	printf("URL for update oui.txt is %s \n",config[Url]);
-	exit( EXIT_SUCCESS );
-}
-void mac_lookup(void) {
-	char temp[512];
-	FILE *file = fopen(strcat(config[Folder],"/oui.txt"), "r");
+}    
 
-	if (!file) {
-		fprintf(stderr, "Error opening file \n");
-		exit( EXIT_FAILURE );
-	}
-	while(fgets(temp,sizeof(temp),file) != NULL) {
-		char oui[7];
-		if ((strstr(temp, globalArgs.macAddress)) != NULL) {
-			strncpy(oui,temp,6);
-			printf("%s %s",oui,temp+20);
-			break;
-		}
-	}
-	
-	if(file) {
-		fclose(file);
-	}
+void print_info(void) 
+{
+    printf("Folder ieee-data is %s \n",config[Folder]);
+    printf("URL for update oui.txt is %s \n",config[Url]);
 }
-void update_oui(void) {
-	char str[255];
-	/* 1 - get oui.txt from web */
-	strcpy(str, "curl ");
-	strcat(str, config[Url]);
-	strcat(str, " | bzip2 -d > /tmp/oui.txt");
-	printf("%s\n",str);
-	
-	FILE * f = popen(str,"r");
-	if (f==0) {
-		fprintf(stderr, "Could not update oui.txt \n");
-		exit(EXIT_FAILURE);
-	}
-	pclose(f);
-	
-	/* 2 - copy oui.txt into directory from config */ 
-	strcpy(str,"cp /tmp/oui.txt ");
-	strcat(str, config[Folder]);
-	strcat(str, "oui.txt");
-	f = popen(str,"r");
-	if (f==0) {
-		fprintf(stderr,"Could not copy oui.txt into directory \n");
-		exit( EXIT_FAILURE );
-	}
-	pclose(f);
+
+void mac_lookup(void) 
+{
+    char temp[512];
+    FILE *file = fopen(strcat(config[Folder],"/oui.txt"), "r");
+
+    if (!file) {
+        fprintf(stderr, "Error opening file \n");
+        exit( EXIT_FAILURE );
+    }
+    while(fgets(temp,sizeof(temp),file) != NULL) {
+        char oui[7];
+        if ((strstr(temp, globalArgs.macAddress)) != NULL) {
+            strncpy(oui,temp,6);
+            printf("%s %s",oui,temp+20);
+            break;
+        }
+    }
+    
+    if(file) {
+        fclose(file);
+    }
 }
-int main(int argc, char *argv[] ) {
-	opncfg();
-	//printf("folder:   %s\n", config[Folder]);
-	//printf("url:    %s\n", config[Url]);
-	int opt = 0;
-	/* Init globalArgs before works */
-	globalArgs.updOui = 0;  /* False */
-	globalArgs.configure = 0;
-	globalArgs.usage = 0;
-	globalArgs.information = 0;
-	globalArgs.macAddress = NULL;
 
-	while((opt = getopt(argc, argv, optString)) != -1) {
-			switch(opt) {
-				case 'u':globalArgs.updOui = 1; break;
-				case 'c':globalArgs.configure = 1; break;
-				case 'i':globalArgs.information = 1; break;
-				case 'h':globalArgs.usage = 1; break;
-				case 'v':printf("Maclookup v %s \n", VERSION); exit(0);
-				case '?':globalArgs.usage = 2; break;
-				default: globalArgs.usage = 2; break;
-			}
-	}
-	if (globalArgs.updOui == 1)	{
-		update_oui();
-		exit(0);
-	}
-	if (globalArgs.information == 1) {
-		print_info();
-		exit(0);
-	}
-	if (globalArgs.usage == 2) {
-		print_usage(stderr);
-		fputs("Try `maclookup -h' for more information.\n",stderr);
-		exit(1);
-	}
-	if ( globalArgs.usage == 1)	{
-		print_usage(stdout);
-		fputs( "OPTIONS: \n",stdout);
-		fputs( " -c\tcreate config file\n",stdout);
-		fputs( " -i\tdisplay info from configuration file\n",stdout);
-		fputs( " -u\tupdate oui.txt from web\n",stdout);
-		fputs( " -v\tdisplay program version\n",stdout);
-		fputs( " -h\tdisplay this help\n",stdout);
-		exit(0);
-	}
+void update_oui(void) 
+{
+    char str[255];
+    /* 1 - get oui.txt from web */
+    strcpy(str, "curl ");
+    strcat(str, config[Url]);
+    strcat(str, " | bzip2 -d > /tmp/oui.txt");
+    printf("%s\n",str);
+    
+    FILE * f = popen(str,"r");
+    if (f==0) {
+        fprintf(stderr, "Could not update oui.txt \n");
+        exit(EXIT_FAILURE);
+    }
+    pclose(f);
+    
+    /* 2 - copy oui.txt into directory from config */ 
+    strcpy(str,"cp /tmp/oui.txt ");
+    strcat(str, config[Folder]);
+    strcat(str, "oui.txt");
+    f = popen(str,"r");
+    if (f==0) {
+        fprintf(stderr,"Could not copy oui.txt into directory \n");
+        exit( EXIT_FAILURE );
+    }
+    pclose(f);
+}
 
-	globalArgs.macAddress = argv[optind];
-	if (globalArgs.macAddress) {
-		globalArgs.macAddress = mac_sanitize(globalArgs.macAddress);
-		if (strlen(globalArgs.macAddress)<6) {
-			fputs("MAC must be 6 char min\n",stderr);
-			print_usage(stderr);
-			exit(EXIT_FAILURE);
-		}
-		mac_lookup();
-	}
-	return (EXIT_SUCCESS);
+int main(int argc, char *argv[] ) 
+{
+    int opt = 0;
+    /* Init globalArgs before works */
+    globalArgs.updOui = 0;  /* False */
+    globalArgs.configure = 0;
+    globalArgs.usage = 0;
+    globalArgs.information = 0;
+    globalArgs.macAddress = NULL;
+
+    while((opt = getopt(argc, argv, optString)) != -1) {
+            switch(opt) {
+                case 'u':globalArgs.updOui = 1; break;
+                case 'c':globalArgs.configure = 1; break;
+                case 'i':globalArgs.information = 1; break;
+                case 'h':globalArgs.usage = 1; break;
+                case 'v':printf("Maclookup v %s \n", VERSION); exit(0);
+                case '?':globalArgs.usage = 2; break;
+                default: globalArgs.usage = 2; break;
+            }
+    }
+    if (globalArgs.usage == 2) {
+        print_usage(stderr);
+        fputs("Try `maclookup -h' for more information.\n",stderr);
+        exit(EXIT_FAILURE);
+    }
+    if ( globalArgs.usage == 1) {
+        print_usage(stdout);
+        fputs( "OPTIONS: \n",stdout);
+        fputs( " -c\tcreate config file\n",stdout);
+        fputs( " -i\tdisplay info from configuration file\n",stdout);
+        fputs( " -u\tupdate oui.txt from web\n",stdout);
+        fputs( " -v\tdisplay program version\n",stdout);
+        fputs( " -h\tdisplay this help\n",stdout);
+        exit(EXIT_SUCCESS);
+    }
+    open_conf();
+    if (globalArgs.updOui == 1) {
+        update_oui();
+        exit(EXIT_SUCCESS);
+    }
+    if (globalArgs.information == 1) {
+        print_info();
+        exit( EXIT_SUCCESS );
+    }
+
+    globalArgs.macAddress = argv[optind];
+    if (globalArgs.macAddress) {
+        globalArgs.macAddress = mac_sanitize(globalArgs.macAddress);
+        if (strlen(globalArgs.macAddress)<6) {
+            fputs("MAC must be 6 char min\n",stderr);
+            print_usage(stderr);
+            exit(EXIT_FAILURE);
+        }
+        mac_lookup();
+    }
+    return (EXIT_SUCCESS);
 }
